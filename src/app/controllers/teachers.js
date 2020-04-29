1fc31d0c44ace6b96159ff987b6e6d61cb95ee53
Cássia <cascrisbern@gmail.com>
2020-04-28 21:40:47 -0300

    fix scholarity output

--- src/app/controllers/teachers.js
+++ src/app/controllers/teachers.js
@@ -49,16 +49,30 @@ module.exports = {
   },
 
   show(req, res) {
+    console.log("teachers controller show() was  called")
+
     Teacher.find(req.params.id, (teacher) => {
       if(!teacher) return res.send("Teacher was not found!")
+
+      if(!(teacher.avatar_url.startsWith('http://') || teacher.avatar_url.startsWith('https://') )) {
+        return res.status(404).send("Teacher does not have a correct avatar_url!")
+      }
 
       teacher.age = age(teacher.birth)
       teacher.services = teacher.services.split(",")
       teacher.created_at = date(teacher.created_at).format
 
-      return res.render("teachers/show", {teacher})
+     return res.render("teachers/show", {
+       teacher, 
+      options: {
+      high_school: "Ensino Médio Completo",
+      higher_education: "Ensino Superior Completo",
+      master_degree: "Mestrado",
+      doctorate_degree: "Doutorado"
+    } })
     })
-  },
+
+  },    
 
   put(req, res) {
     const keys = Object.keys(req.body) //it will be an array of the keys of the object
